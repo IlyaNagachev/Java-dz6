eadf91a2bb6549e0854c26d29176ce261f961a3c
Илья Нагачев <Ilya>
2024-02-29 14:33:51 +0300

    Second commit

--- Main.java
+++ Main.java
@@ -1,4 +1,17 @@
 public class Main {
 
-    
+    public static void main(String[] args) {
+        Notebooks nb1 = new Notebooks("Asus", "ZenBook", 8, 256, 11, 40000, "Черный");
+        Notebooks nb2 = new Notebooks("MSI", "Katana", 16, 512, 10, 110000, "Серый");
+        Notebooks nb3 = new Notebooks("Gigabyte", "G5", 16, 1024, 11, 80000, "Черный");
+        Notebooks nb4 = new Notebooks("Asus", "TUF", 12, 512, 11, 60000, "Черный");
+        Notebooks nb5 = new Notebooks("Acer", "Nitro", 32, 2048, 11, 150000, "Белый");
+        System.out.println(nb1);
+        System.out.println(nb2);
+        System.out.println(nb3);
+        System.out.println(nb4);
+        System.out.println(nb5);
+
+
+    }
 }
--- Notebooks.java
+++ Notebooks.java
@@ -1,3 +1,83 @@
 public class Notebooks {
-    
+    private String name;
+    private String model;
+    private int ram;
+    private int hardDrive;
+    private int oc;
+    private int price;
+    private String color;
+
+public Notebooks(String name, String model, int ram, int hardDrive, int oc, int price, String color){
+    this.name = name;
+    this.model = model;
+    this.ram = ram;
+    this.hardDrive = hardDrive;
+    this.oc = oc;
+    this.price = price;
+    this.color = color;
+}
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public int getRam() {
+        return ram;
+    }
+
+    public void setRam(int ram) {
+        this.ram = ram;
+    }
+
+    public int getHardDrive() {
+        return hardDrive;
+    }
+
+    public void setHardDrive(int hardDrive) {
+        this.hardDrive = hardDrive;
+    }
+
+    public int getOc() {
+        return oc;
+    }
+
+    public void setOc(int oc) {
+        this.oc = oc;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    @Override
+    public String toString() {
+        return "Notebooks{" + "Производитель='" + name + '\'' + "Марка='" + model + '\'' + "ОЗУ='" + ram + '\''  + "Накопитель='" + hardDrive +
+        "OC='" + oc + '\'' + '\'' + "Стоимость='" + price + '\'' + ", цвет='" + color + '}';
+    }
+
 }
